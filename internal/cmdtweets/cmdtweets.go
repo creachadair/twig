6f8e4cfb6bed83c095c1fc8f2bd3b1faed4394fd
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-09-04 11:36:53 -0700

    Implement tweets search.

--- internal/cmdtweets/cmdtweets.go
+++ internal/cmdtweets/cmdtweets.go
@@ -51,5 +51,64 @@ As a special case, :field is shorthand for "tweet:field".
 				return nil
 			},
 		},
+
+		{
+			Name:  "search",
+			Usage: "[-page token] -query q [field-spec...]",
+			Flags: command.FlagSet("search"),
+			Help: `
+Search for recent tweets matching the specified query.
+
+A field spec has the form type:field, e.g., "tweet:entities".
+As a special case, :field is shorthand for "tweet:field".
+
+If the results span multiple pages, use -page to set the
+page token to resume searching from.
+`,
+
+			Run: func(ctx *command.Context, args []string) error {
+				if searchQuery == "" {
+					fmt.Fprintln(ctx, "Error: a search -query must be set")
+					return command.FailWithUsage(ctx, args)
+				}
+				parsed := config.ParseArgs(args, "tweet")
+				if len(parsed.Keys) != 0 {
+					fmt.Fprintf(ctx, "Error: extra arguments after query %v\n", parsed.Keys)
+					return command.FailWithUsage(ctx, args)
+				}
+
+				cli, err := ctx.Config.(*config.Config).NewBearerClient()
+				if err != nil {
+					return fmt.Errorf("creating client: %w", err)
+				}
+				rsp, err := tweets.SearchRecent(searchQuery, &tweets.SearchOpts{
+					PageToken:  pageToken,
+					MaxResults: maxResults,
+					Optional:   parsed.Fields,
+				}).Invoke(context.Background(), cli)
+				if err != nil {
+					return err
+				}
+				data, err := json.Marshal(rsp.Reply)
+				if err != nil {
+					return err
+				}
+				fmt.Println(string(data))
+				return nil
+			},
+		},
 	},
+}
+
+var (
+	maxResults  int
+	pageToken   string
+	searchQuery string
+)
+
+func init() {
+	fs := Command.Commands[1].Flags
+	fs.IntVar(&maxResults, "max-results", 0, "Maximum results to request (10..100)")
+	fs.StringVar(&pageToken, "page", "", "Page token to resume search")
+	fs.StringVar(&searchQuery, "query", "", "Search query (required)")
 }
