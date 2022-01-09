7880b968d5901917c274958adb740aefca3c39b1
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-01-09 10:10:05 -0800

    stream: Rework printing of results
    Promote the "search" subcommand to the root of the package.  Rework how flags
    are plumbed in to the command. Print only the matching tweets, not the wrapper.

--- internal/cmdstream/cmdstream.go
+++ internal/cmdstream/cmdstream.go
@@ -5,6 +5,7 @@ package cmdstream
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 
 	"github.com/creachadair/command"
@@ -13,39 +14,38 @@ import (
 )
 
 var Command = &command.C{
-	Name: "stream",
-	Help: "Commands to query the stream APIs.",
-	Commands: []*command.C{
-		{
-			Name:  "search",
-			Usage: "field-spec...",
-			Help:  "Search for matches to the current query rules",
-			Run: func(env *command.Env, args []string) error {
-				parsed := config.ParseArgs(args, "tweet")
-				if len(parsed.Keys) != 0 {
-					fmt.Fprintf(env, "Error: extra arguments after query %v\n", parsed.Keys)
-					return command.FailWithUsage(env, args)
-				}
-				cli, err := env.Config.(*config.Config).NewClient()
+	Name:  "stream",
+	Usage: "field-spec...",
+	Help:  "Stream matches to the current query rules.",
+	SetFlags: func(_ *command.Env, fs *flag.FlagSet) {
+		fs.IntVar(&opts.maxResults, "max", 0, "Maximum results to fetch (0 means all)")
+	},
+	Run: func(env *command.Env, args []string) error {
+		parsed := config.ParseArgs(args, "tweet")
+		if len(parsed.Keys) != 0 {
+			fmt.Fprintf(env, "Error: extra arguments after query %v\n", parsed.Keys)
+			return command.FailWithUsage(env, args)
+		}
+		cli, err := env.Config.(*config.Config).NewClient()
+		if err != nil {
+			return fmt.Errorf("creating client: %w", err)
+		}
+		return tweets.SearchStream(func(rsp *tweets.Reply) error {
+			for _, tw := range rsp.Tweets {
+				data, err := json.Marshal(tw)
 				if err != nil {
-					return fmt.Errorf("creating client: %w", err)
+					return err
 				}
-				opts.Optional = parsed.Fields
-				return tweets.SearchStream(func(rsp *tweets.Reply) error {
-					data, err := json.Marshal(rsp.Reply)
-					if err != nil {
-						return err
-					}
-					fmt.Println(string(data))
-					return nil
-				}, &opts).Invoke(context.Background(), cli)
-			},
-		},
+				fmt.Println(string(data))
+			}
+			return nil
+		}, &tweets.StreamOpts{
+			MaxResults: opts.maxResults,
+			Optional:   parsed.Fields,
+		}).Invoke(context.Background(), cli)
 	},
 }
 
-var opts tweets.StreamOpts
-
-func init() {
-	Command.Flags.IntVar(&opts.MaxResults, "max-results", 0, "Maximum results to fetch")
+var opts struct {
+	maxResults int
 }
