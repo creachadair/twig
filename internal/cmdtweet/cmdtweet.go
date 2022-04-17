8a54db850fa736b2c1c90ec38136767febad7c23
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-04-17 10:19:06 -1000

    Use API v2 methods for many tweet edits.

--- internal/cmdtweet/cmdtweet.go
+++ internal/cmdtweet/cmdtweet.go
@@ -10,8 +10,10 @@ import (
 
 	"github.com/creachadair/command"
 	"github.com/creachadair/twig/config"
+	"github.com/creachadair/twitter/edit"
 	"github.com/creachadair/twitter/ostatus"
 	"github.com/creachadair/twitter/types"
+	"github.com/creachadair/twitter/users"
 )
 
 var Command = &command.C{
@@ -23,40 +25,40 @@ var Command = &command.C{
 			Name:  "delete",
 			Usage: "id",
 			Help:  "Delete the tweet with the specified ID.",
-			Run: runWithID(func(id string) ostatus.Query {
-				return ostatus.Delete(id, nil)
+			Run: runWithID(func(_, tweetID string) edit.Query {
+				return edit.DeleteTweet(tweetID)
 			}),
 		},
 		{
 			Name:  "like",
-			Usage: "id",
-			Help:  "Like the tweet with the specified ID.",
-			Run: runWithID(func(id string) ostatus.Query {
-				return ostatus.Like(id, nil)
+			Usage: "user-id id",
+			Help:  "Mark tweet id as liked by user-id.",
+			Run: runWithID(func(userID, tweetID string) edit.Query {
+				return edit.Like(userID, tweetID)
 			}),
 		},
 		{
 			Name:  "unlike",
-			Usage: "id",
-			Help:  "Un-like the tweet with the specified ID.",
-			Run: runWithID(func(id string) ostatus.Query {
-				return ostatus.Unlike(id, nil)
+			Usage: "user-id id",
+			Help:  "Unmark tweet id as liked by user-id.",
+			Run: runWithID(func(userID, tweetID string) edit.Query {
+				return edit.Unlike(userID, tweetID)
 			}),
 		},
 		{
 			Name:  "retweet",
-			Usage: "id",
-			Help:  "Retweet the tweet with the specified ID.",
-			Run: runWithID(func(id string) ostatus.Query {
-				return ostatus.Retweet(id, nil)
+			Usage: "user-id id",
+			Help:  "Retweet tweet id from user.id.",
+			Run: runWithID(func(userID, tweetID string) edit.Query {
+				return edit.Retweet(userID, tweetID)
 			}),
 		},
 		{
 			Name:  "unretweet",
-			Usage: "id",
-			Help:  "Un-retweet the tweet with the specified ID.",
-			Run: runWithID(func(id string) ostatus.Query {
-				return ostatus.Unretweet(id, nil)
+			Usage: "user-id id",
+			Help:  "Un-retweet tweet id from user-id.",
+			Run: runWithID(func(userID, tweetID string) edit.Query {
+				return edit.Unretweet(userID, tweetID)
 			}),
 		},
 	},
@@ -107,21 +109,29 @@ var cmdCreate = &command.C{
 	},
 }
 
-func runWithID(newQuery func(id string) ostatus.Query) func(*command.Env, []string) error {
+func runWithID(newQuery func(uid, tid string) edit.Query) func(*command.Env, []string) error {
 	return func(env *command.Env, args []string) error {
 		if len(args) != 1 || args[0] == "" {
 			return command.FailWithUsage(env, args)
 		}
-
-		cli, err := env.Config.(*config.Config).NewClient()
+		cfg := env.Config.(*config.Config)
+		cli, err := cfg.NewClient()
 		if err != nil {
 			return fmt.Errorf("creating client: %w", err)
 		}
 
-		rsp, err := newQuery(args[0]).Invoke(context.Background(), cli)
+		ctx := context.Background()
+		var uid string
+		if rsp, err := users.LookupByName(cfg.AuthUser, nil).Invoke(ctx, cli); err != nil {
+			return fmt.Errorf("resolving user: %w", err)
+		} else {
+			uid = rsp.Users[0].ID
+		}
+
+		rsp, err := newQuery(uid, args[0]).Invoke(context.Background(), cli)
 		if err != nil {
 			return err
 		}
-		return config.PrintJSON(rsp.Tweets)
+		return config.PrintJSON(rsp)
 	}
 }
